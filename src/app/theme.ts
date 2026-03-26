8eec56b462d2b2b2f4385edc0df2f6f3236e62e8
ZM <zmxidn@gmail.com>
2026-03-27 03:18:39 +0700

    fix: adjust Snackbar position to bottom and set autoHideDuration

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,5 +1,4 @@
 import { createTheme } from "@mui/material/styles";
-import { AuthProvider } from "@/context/AuthContext";
 
 export const Theme = createTheme({
     palette: {
@@ -303,9 +302,10 @@ export const Theme = createTheme({
         MuiSnackbar: {
             defaultProps: {
                 anchorOrigin: {
-                    vertical: "top",
+                    vertical: "bottom",
                     horizontal: "right",
                 },
+                autoHideDuration: 6000,
             },
 
             styleOverrides: {
