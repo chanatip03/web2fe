1f9be478bb8e50a5b163134899eb64eba33a27e0
V0yager <charnatip15@gmail.com>
2026-04-12 08:44:55 +0700

    add response to logout

--- src/services/auth/repository.ts
+++ src/services/auth/repository.ts
@@ -87,5 +87,10 @@ export class AuthRepository implements IAuthRepository {
       method: "POST",
       credentials: "include",
     });
+    if (!res.ok) {
+      throw new Error("Logout failed");
+    }
+
+    return res.json();
   }
 }
