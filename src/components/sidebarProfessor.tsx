04578d597a1c1acd20fe88004f4bce2380fa9069
ZM <zmxidn@gmail.com>
2026-04-17 16:32:36 +0700

    refactor: Remove Classroom Setting component and update Syllabus component with edit functionality and snackbar notifications

--- src/components/sidebarProfessor.tsx
+++ src/components/sidebarProfessor.tsx
@@ -32,11 +32,6 @@ const sidebarProfessorItems = [
     href: "/scorebook",
     icon: <EmojiEventsOutlinedIcon />,
   },
-  {
-    name: "Setting",
-    href: "/setting",
-    icon: <SettingsOutlinedIcon />,
-  },
 ];
 
 export const SidebarProfessor = ({ classroomName }: SidebarProfessorProps) => {
